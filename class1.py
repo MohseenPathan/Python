b1db41d890948ad616af243b57db7ec3b295a490
MohseenPathan <mosinpathan1985@gmail.com>
2018-10-21 13:26:42 +0530

    this is initial commit

---
+++ class1.py
@@ -0,0 +1,10 @@
+""""
+This is class 01 file
+1)we set up python2.7,git,phycharm
+2)created github repo for batch
+3)created virtual env.
+4)setuo pycharm project
+5)created the file and printed hello world
+"""
+
+print ("hello world")
